8428d102a9262e08625e0639a55236c0b76980d7
Russell28 <russellsalioan@gmail.com>
2026-03-14 11:01:29 +0800

    Fixes #4
    Trips basic CRUD completed. Exposed Trips API

--- src/RefillingStation.Api/Program.cs
+++ src/RefillingStation.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
 using RefillingStation.Api.Data;
+using RefillingStation.Api.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,5 +42,75 @@ app.MapGet("/health", () => Results.Ok(new
     service = "RefillingStation.Api",
     utc = DateTime.UtcNow
 }));
+
+// Trips API
+app.MapGet("/trips", async (AppDbContext db) =>
+    await db.Trips
+        .OrderByDescending(t => t.Date)
+        .ThenBy(t => t.TripNumber)
+        .ToListAsync()
+);
+app.MapGet("/trips/{id}", async (int id, AppDbContext db) =>
+    await db.Trips.FindAsync(id) is Trip trip
+        ? Results.Ok(trip)
+        : Results.NotFound()
+);
+app.MapPost("/trips", async (Trip trip, AppDbContext db) =>
+{
+    db.Trips.Add(trip);
+    await db.SaveChangesAsync();
+    return Results.Created($"/trips/{trip.Id}", trip);
+});
+app.MapPut("/trips/{id}", async (int id, Trip inputTrip, AppDbContext db) =>
+{
+    var trip = await db.Trips.FindAsync(id);
+    if (trip is null) return Results.NotFound();
+
+    // Update all properties
+    trip.Date = inputTrip.Date;
+    trip.TripNumber = inputTrip.TripNumber;
+    trip.Segment = inputTrip.Segment;
+
+    trip.TimeStarted = inputTrip.TimeStarted;
+    trip.TimeEnded = inputTrip.TimeEnded;
+
+    trip.Source = inputTrip.Source;
+    trip.TripType = inputTrip.TripType;
+    trip.EmployeeName = inputTrip.EmployeeName;
+    trip.CustomerCategory = inputTrip.CustomerCategory;
+
+    trip.CollectedQty = inputTrip.CollectedQty;
+    trip.LoadedQty = inputTrip.LoadedQty;
+    trip.DeliveredQty = inputTrip.DeliveredQty;
+
+    trip.FreeQty = inputTrip.FreeQty;
+    trip.ToBePaidQty = inputTrip.ToBePaidQty;
+    trip.ActualPaidQty = inputTrip.ActualPaidQty;
+
+    trip.ReturnedQty = inputTrip.ReturnedQty;
+    trip.ReplacementQty = inputTrip.ReplacementQty;
+
+    trip.PricePerGallon = inputTrip.PricePerGallon;
+    trip.ActualCashCollected = inputTrip.ActualCashCollected;
+
+    trip.RelatedTripId = inputTrip.RelatedTripId;
+    trip.AdjustmentReason = inputTrip.AdjustmentReason;
+
+    trip.Notes = inputTrip.Notes;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/trips/{id}", async (int id, AppDbContext db) =>
+{
+    var trip = await db.Trips.FindAsync(id);
+    if (trip is null) return Results.NotFound();
+
+    db.Trips.Remove(trip);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 
 app.Run();
