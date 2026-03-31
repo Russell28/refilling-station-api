570f7655ad8cb203cb2d65074b951700597b9314
Russell28 <russellsalioan@gmail.com>
2026-03-31 17:41:02 +0800

    WR-34 feat(api): Add common Import error

---
+++ src/RefillingStation.Api/Common/Import/ImportError.cs
@@ -0,0 +1,8 @@
+﻿namespace RefillingStation.Api.Common.Import
+{
+    public class ImportError
+    {
+        public int RowNumber { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
---
+++ src/RefillingStation.Api/Common/Import/ImportResult.cs
@@ -0,0 +1,11 @@
+﻿
+namespace RefillingStation.Api.Common.Import
+{
+    public class ImportResult
+    {
+        public int TotalRows { get; set; }
+        public int InsertedRows { get; set; }
+        public int FailedRows { get; set; }
+        public List<ImportError> Errors { get; set; } = new();
+    }
+}
