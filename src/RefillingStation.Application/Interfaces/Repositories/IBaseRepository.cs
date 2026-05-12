1dd29ce6f935d1a04a4ec68a2e7bbbf92e65ed44
Russell28 <russellsalioan@gmail.com>
2026-05-12 10:05:08 +0800

    WR-46D feat: Added Base Repository

---
+++ src/RefillingStation.Application/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace RefillingStation.Application.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Remove(T entity);
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ src/RefillingStation.Infrastructure/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RefillingStation.Application.Interfaces.Repositories;
+
+namespace RefillingStation.Infrastructure.Persistence.Repositories
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+
+        protected BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public virtual async Task<T?> GetByIdAsync(int id)
+            => await _context.Set<T>().FindAsync(id);
+
+        public virtual async Task<List<T>> GetAllAsync()
+            => await _context.Set<T>()
+                .Take(100)
+                .OrderByDescending(e => EF.Property<int>(e, "Id"))
+                .ToListAsync();
+
+        public virtual async Task AddAsync(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public virtual void Remove(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+            _context.Set<T>().Remove(entity);
+        }
+
+        public virtual async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
