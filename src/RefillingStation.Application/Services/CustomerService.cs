c38984cf1c14ad40b6e73472ed3a55bd08588d6e
Russell28 <russellsalioan@gmail.com>
2026-05-12 17:48:39 +0800

    WR-46C feat: Implement CustomerService

--- src/RefillingStation.Application/Services/CustomerService.cs
+++ src/RefillingStation.Application/Services/CustomerService.cs
@@ -1,12 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using FluentValidation;
+using RefillingStation.Application.DTOs.Customers;
+using RefillingStation.Application.Interfaces.Repositories;
+using RefillingStation.Application.Interfaces.Services;
+using RefillingStation.Domain.Entities;
 
 namespace RefillingStation.Application.Services
 {
-    public class CustomerService
+    public class CustomerService : ICustomerService
     {
+        private readonly ICustomerRepository _repository;
+        private readonly IValidator<CustomerCreateRequest> _validator;
+
+        public CustomerService(
+            ICustomerRepository repository,
+            IValidator<CustomerCreateRequest> validator)
+        {
+            _repository = repository;
+            _validator = validator;
+        }
+        public async Task<List<CustomerListItemResponse>> GetAllAsync()
+        {
+            var customers = await _repository.GetAllAsync();
+
+            return customers
+                .Select(x => new CustomerListItemResponse
+                (
+                    x.Id,
+                    x.Name
+                ))
+                .ToList();
+        }
+
+        public async Task<CustomerListItemResponse> GetByIdAsync(int id)
+        {
+            var customer = await _repository.GetByIdAsync(id);
+
+            if (customer is null)
+                throw new Exception("Customer not found.");
+
+            return new CustomerListItemResponse(
+                customer.Id,
+                customer.Name
+            );
+        }
+
+        public async Task<int> CreateAsync(CustomerCreateRequest request)
+        {
+            var validation = await _validator.ValidateAsync(request);
+
+            if (!validation.IsValid)
+                throw new ValidationException(validation.Errors);
+
+            var customer = new Customer
+            {
+                Name = request.Name,
+            };
+
+            await _repository.AddAsync(customer);
+            await _repository.SaveChangesAsync();
+
+            return customer.Id;
+        }
+
+        public async Task UpdateAsync(int id, CustomerCreateRequest request)
+        {
+            var validation = await _validator.ValidateAsync(request);
+
+            if (!validation.IsValid)
+                throw new ValidationException(validation.Errors);
+
+            var customer = await _repository.GetByIdAsync(id);
+
+            if (customer is null)
+                throw new Exception("Customer not found.");
+
+            customer.Name = request.Name;
+
+            await _repository.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var customer = await _repository.GetByIdAsync(id);
+
+            if (customer is null)
+                throw new Exception("Customer not found.");
+
+            _repository.Remove(customer);
+
+            await _repository.SaveChangesAsync();
+        }
     }
 }
