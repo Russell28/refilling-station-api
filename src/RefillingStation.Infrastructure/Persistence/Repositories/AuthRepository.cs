3228aae019a49823eae181f9449b6be9a81f2adb
Russell28 <russellsalioan@gmail.com>
2026-05-12 10:14:23 +0800

    WR-46D feat: Implement AuthRepository

---
+++ src/RefillingStation.Infrastructure/Persistence/Repositories/AuthRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RefillingStation.Application.Interfaces.Repositories;
+using RefillingStation.Domain.Entities;
+
+namespace RefillingStation.Infrastructure.Persistence.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly AppDbContext _context; // Dependency injection of the database context
+
+        public AuthRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User?> GetByUsernameAsync(string username)
+            => await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+    }
+}
