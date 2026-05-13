d6e35002e186e591b90b4df1159571de3482e51a
Russell28 <russellsalioan@gmail.com>
2026-05-13 14:35:34 +0800

    WR-46F feat: Add ExpensesController

---
+++ src/RefillingStation.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RefillingStation.Application.DTOs.Expenses;
+using RefillingStation.Application.Interfaces.Services;
+
+namespace RefillingStation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly IExpenseService _service;
+
+        public ExpensesController(IExpenseService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _service.GetAllAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ExpenseCreateRequest request)
+        {
+            var id = await _service.CreateAsync(request);
+
+            return Ok(id);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, ExpenseCreateRequest request)
+        {
+            await _service.UpdateAsync(id, request);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+}
