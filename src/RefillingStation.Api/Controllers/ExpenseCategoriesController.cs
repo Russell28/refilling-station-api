c24e21eb089a40d202d14c77ac1bdbbe0020ba39
Russell28 <russellsalioan@gmail.com>
2026-05-13 14:27:53 +0800

    WR-46F feat: Add ExpenseCategoriesController

---
+++ src/RefillingStation.Api/Controllers/ExpenseCategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RefillingStation.Application.Interfaces.Services;
+
+namespace RefillingStation.Api.Controllers
+{
+    [Route("api/expense-categories")]
+    [ApiController]
+    public class ExpenseCategoriesController : ControllerBase
+    {
+        private readonly IExpenseCategoryService _service;
+
+        public ExpenseCategoriesController(IExpenseCategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _service.GetAllAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+
+            return Ok(result);
+        }
+    }
+}
