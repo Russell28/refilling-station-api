dbcb997d4c5318ea959a5fa4c2d7f5e1bee2ca85
Russell28 <russellsalioan@gmail.com>
2026-05-13 14:21:42 +0800

    WR-46F feat: Add EmployeesController

---
+++ src/RefillingStation.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RefillingStation.Application.DTOs.Employees;
+using RefillingStation.Application.Interfaces.Services;
+
+namespace RefillingStation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _service;
+
+        public EmployeesController(IEmployeeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _service.GetAllAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+
+            return Ok(result);
+        }
+    }
+}
