bf012ffd702e7162827b2f4b2c26e5b32ad95c41
Russell28 <russellsalioan@gmail.com>
2026-05-13 13:32:17 +0800

    WR-46F feat: Add CustomersController

---
+++ src/RefillingStation.Api/Controllers/CustomersController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RefillingStation.Application.DTOs.Customers;
+using RefillingStation.Application.Interfaces.Services;
+
+namespace RefillingStation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _service;
+
+        public CustomersController(ICustomerService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _service.GetAllAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CustomerCreateRequest request)
+        {
+            var id = await _service.CreateAsync(request);
+
+            return Ok(id);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, CustomerCreateRequest request)
+        {
+            await _service.UpdateAsync(id, request);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+}
