bba95dadeb386de4152265ecbd9672c54a639d81
Russell28 <russellsalioan@gmail.com>
2026-05-13 13:00:10 +0800

    WR-46F feat: Add AuthController

---
+++ src/RefillingStation.Api/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using RefillingStation.Application.DTOs.Auth;
+using Microsoft.AspNetCore.Mvc;
+using RefillingStation.Application.Interfaces;
+using System.Security.Claims;
+
+namespace RefillingStation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _service;
+
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var result = await _service.LoginAsync(request);
+
+            return Ok(result);
+        }
+
+        [HttpGet("Me")]
+        public async Task<IActionResult> Me()
+        {
+            var userId = User.FindFirstValue(
+                ClaimTypes.NameIdentifier);
+
+            var username = User.Identity?.Name;
+
+            var role = User.FindFirstValue(
+                ClaimTypes.Role);
+
+            if (userId is null ||
+                username is null ||
+                role is null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new AuthUserInfoResponse(
+                userId,
+                username,
+                role
+            ));
+        }
+    }
+}
