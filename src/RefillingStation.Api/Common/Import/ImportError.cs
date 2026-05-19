e27a6d3bcf4820a9a10456ec49406aa11c6cbb4d
Russell28 <russellsalioan@gmail.com>
2026-05-19 12:29:31 +0800

    WR-46F chore: Remove remaining minimal API endpoint mappings, services, dtos

--- src/RefillingStation.Api/Common/Import/ImportError.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RefillingStation.Api.Common.Import
-{
-    public class ImportError
-    {
-        public int RowNumber { get; set; }
-        public string Message { get; set; } = string.Empty;
-    }
-}
