5d901b9cbf7c7ce004b249ede22ef3d596737efb
Russell28 <russellsalioan@gmail.com>
2026-06-08 23:32:17 +0800

    WR-58 chore: Removed test code on CustomerService

--- src/RefillingStation.Infrastructure/Persistence/Repositories/CustomerRepository.cs
+++ src/RefillingStation.Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -9,14 +9,11 @@ namespace RefillingStation.Infrastructure.Persistence.Repositories
         public CustomerRepository(AppDbContext context) : base(context) { }
 
         public override async Task<List<Customer>> GetAllAsync()
-        { 
-            Console.WriteLine("DB HIT: Fetching customers from database...");
-            return await _context.Customers
+            => await _context.Customers
                 .AsNoTracking()
                 .OrderBy(x => x.Name.Contains("Other")) // Push "Other" to the end of the list
                 .ThenBy(x => x.Name)
                 .Take(100)
                 .ToListAsync();
-        }
     }
 }
