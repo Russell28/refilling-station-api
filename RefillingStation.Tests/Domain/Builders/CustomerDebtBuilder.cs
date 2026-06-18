b3d6a16f9bedc8b407e8dcd17c7d11bce4aa7f93
Russell28 <russellsalioan@gmail.com>
2026-06-18 15:28:22 +0800

    WR-48 test: add CustomerDebtService application unit tests

---
+++ RefillingStation.Tests/Domain/Builders/CustomerDebtBuilder.cs
@@ -0,0 +1,33 @@
+﻿using RefillingStation.Domain.Entities;
+
+
+namespace RefillingStation.Tests.Domain.Builders
+{
+    public class CustomerDebtBuilder
+    {
+        private CustomerDebtEntry _debt =
+            new CustomerDebtEntry
+            {
+                Id = 1,
+                Date = DateOnly.FromDateTime(DateTime.UtcNow),
+                Amount = 100
+            };
+
+        public CustomerDebtBuilder WithAmount(decimal amount)
+        {
+            _debt.Amount = amount;
+            return this;
+        }
+
+        public CustomerDebtBuilder WithCustomer(Customer customer)
+        {
+            _debt.Customer = customer;
+            return this;
+        }
+
+        public CustomerDebtEntry Build()
+        {
+            return _debt;
+        }
+    }
+}
---
+++ RefillingStation.Tests/Services/Builders/CustomerDebtCreateRequestBuilder.cs
@@ -0,0 +1,36 @@
+﻿using RefillingStation.Application.DTOs.CustomerDebts;
+
+namespace RefillingStation.Tests.Services.Builders
+{
+    public class CustomerDebtCreateRequestBuilder
+    {
+        private DateOnly _date = new DateOnly();
+        private int _customerId = 1;
+        private decimal _amount = 0;
+
+        public CustomerDebtCreateRequestBuilder WithAmount(decimal amount)
+        {
+            _amount = amount;
+            return this;
+        }
+
+        public CustomerDebtCreateRequestBuilder WithDate(DateOnly date)
+        {
+            _date = date;
+            return this;
+        }
+
+        public CustomerDebtCreateRequest Build()
+        {
+            var request =
+                new CustomerDebtCreateRequest
+                {
+                    Date = _date,
+                    Amount = _amount,
+                    CustomerId = _customerId
+                };
+
+            return request;
+        }
+    }
+}
